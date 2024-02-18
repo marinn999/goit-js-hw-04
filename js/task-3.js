91de04c95a049a2e72bdc2f680bd6e70edbad4ec
marinn999 <marinacompel@icloud.com>
2024-02-18 22:52:40 +0100

    check

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,22 @@
-function getElementWidth (content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    return totalWidth;
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
